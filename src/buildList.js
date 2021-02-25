1593c5e86d5de57c65d3d5e1ba4f2e10628f5bcd
ianlapham <ianlapham@gmail.com>
2021-02-25 13:38:20 -0500

    update name

--- src/buildList.js
+++ src/buildList.js
@@ -8,7 +8,7 @@ const kovan = require("./tokens/kovan.json");
 module.exports = function buildList() {
   const parsed = version.split(".");
   return {
-    name: "BA non-compliant",
+    name: "BA SEC Non compliant",
     timestamp: new Date().toISOString(),
     version: {
       major: +parsed[0],
